2b03320e03f1c288e0d16d03b2674eb2ea8cd290
Hazim Bakar <hazimbaa@gmail.com>
2025-06-13 21:16:10 +0800

    mainpage morkup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import HomePage from "./components/home";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import HomePage from './components/home';
 import Mainpage from './components/mainpage';
 import './output.css';
-import Manage from "./components/manage";
+import Manage from './components/manage';
 
 const App = () => {
   return (
-    <div className="flex">
-    <div className="flex flex-col items-center justify-center w-64 bg-gray-100 min-h-screen">
-      <h2 className="text-xl font-bold">Sidebar</h2>
-      <a href="/">Dashboard</a>
-      <a href="/home">Home</a>
-      <a href="/manage">Manage</a>
-    </div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/manage" element={<Manage />} />
-      </Routes>
-    </Router>
+    <div className="flex h-screen">
+      <div className="flex flex-col items-center justify-center w-64 bg-gray-100 min-h-screen">
+        <h2 className="text-xl font-bold">Sidebar</h2>
+        <a href="/">Dashboard</a>
+        <a href="/home">Home</a>
+        <a href="/manage">Manage</a>
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/manage" element={<Manage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
--- src/components/Mainpage/index.tsx
+++ src/components/Mainpage/index.tsx
@@ -1,11 +1,20 @@
 const Mainpage = () => {
   return (
-    <div className="flex flex-row w-full h-screen justify-center items-center">
-      <div>This is Dashboard</div>
-      <div>
+    <div className="flex flex-col w-full bg-gray-300">
+    <div className="flex flex-col w-full justify-between items-center h-1/3">
+      <div className="flex gap-4 justify-between items-center w-full px-4 py-2 bg-gray-200 h-12">
         <div>Search</div>
-        <div></div>
+        <div className="flex gap-2">
+          <div>Icon 1</div>
+          <div>Icon 2</div>
+          <div>Icon 3</div>
+        </div>
       </div>
+      <div className="bg-gray-500 w-full h-full flex justify-center items-end">
+        This is Dashboard
+      </div>
+    </div>
+    <div className="flex justify-center items-center">This is Main</div>
     </div>
   );
 };
