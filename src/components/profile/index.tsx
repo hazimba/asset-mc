b8fb2d6a5338a02855835124784ee7157121dd10
Hazim Bakar <hazimbaa@gmail.com>
2025-06-15 01:42:29 +0800

    profile mockup

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,8 +1,40 @@
+import { Tabs, TabsProps, Typography } from "antd";
+import InfoPersonal from "./infoPersonal";
+
+const { Text, Title } = Typography;
+
 const Profile = () => {
+
+  const items: TabsProps['items'] = [
+    {
+      key: "1",
+      label: "Profile",
+      children: <InfoPersonal />
+    },
+    {
+      key: "2",
+      label: "Settings",
+      children: <Text>Settings Content</Text>,
+    },
+    {
+      key: "3",
+      label: "Activity",
+      children: <Text>Activity Content</Text>,
+    },
+  ];
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-300">
-      <h1 className="text-2xl font-bold mb-4">Profile Page</h1>
-      <p className="text-gray-700">This is the Profile page content.</p>
+      <div className="bg-white p-6 rounded-lg shadow-md w-3/4 h-1/2 overflow-y-scroll">
+        <Title level={2} className="font-semibold">Profile</Title>
+        <>
+          <Tabs
+            defaultActiveKey="1"
+            items={items}
+            onChange={(key) => console.log(key)}
+          />
+        </>
+      </div>
     </div>
   );
 };
